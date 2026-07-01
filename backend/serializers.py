5260a7e8051f606a2f37b9c0b36086ab1b21c447
Marvik <slkspot@gmail.com>
2026-07-01 17:30:33 +0300

    user reg

---
+++ backend/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from backend.models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        password = validated_data.pop ('password', None)
+        user = self.Meta.model(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'company', 'position', 'type')
+        extra_kwargs = {'password': {'write_only': True}}
